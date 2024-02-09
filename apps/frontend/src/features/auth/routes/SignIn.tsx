75f20887aaf197d315ed1eb90aad6df254418420
dawsta27 <dstaniszewski00@gmail.com>
2024-02-09 15:05:49 +0100

    feat(frontend): B24-59 update sign-in form to use zod and modularforms; responsive layout

--- apps/frontend/src/features/auth/routes/SignIn.tsx
+++ apps/frontend/src/features/auth/routes/SignIn.tsx
@@ -1,19 +1,40 @@
 import Logo from '@/components/logo/Logo';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Component } from 'solid-js';
 import { A } from '@solidjs/router';
+import { createForm, SubmitHandler, zodForm } from '@modular-forms/solid';
+import { z } from 'zod';
+import { Input } from '@/components/ui/input';
+
+const SignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type SignInForm = z.infer<typeof SignInSchema>;
 
 const SignIn: Component = () => {
+  const [, { Form, Field }] = createForm({validate:zodForm(SignInSchema)});
+  const handleSubmit: SubmitHandler<SignInForm> = (
+    values,
+    event
+  ) => {
+    
+  };
+
   return (
     <div>
       <main>
         <div class='flex h-screen items-center justify-center'>
-          <div class='flex h-1/2 w-1/5 flex-col items-center justify-center rounded-lg border border-gray-100 shadow-lg'>
+          <div class='flex w-full max-w-md flex-col items-center justify-center rounded-lg border border-gray-100 py-6 shadow-lg'>
             <Logo />
 
-            <form id='sign-in-form' class='flex w-full flex-col gap-2 px-4'>
+            <Form
+              id='sign-in-form'
+              class='flex w-full flex-col gap-2 px-4'
+              onSubmit={handleSubmit}
+            >
               <h1 class='font-title mt-2 text-center text-xl font-semibold'>
                 Sign in to inSight
               </h1>
@@ -21,12 +42,30 @@ const SignIn: Component = () => {
               <Label for='email' class='font-semibold'>
                 E-mail
               </Label>
-              <Input type='email' id='email' placeholder='E-mail' />
+              <Field name='email'>
+                {(field, props) => (
+                  <Input
+                    {...props}
+                    type='email'
+                    id='email'
+                    placeholder='E-mail'
+                  />
+                )}
+              </Field>
 
               <Label for='password' class='font-semibold'>
                 Password
               </Label>
-              <Input type='password' id='password' placeholder='Password' />
+              <Field name='password'>
+                {(field, props) => (
+                  <Input
+                    {...props}
+                    type='password'
+                    id='password'
+                    placeholder='Password'
+                  />
+                )}
+              </Field>
 
               <div class='flex justify-end'>
                 <A class='text-sm hover:underline' href=''>
@@ -35,7 +74,7 @@ const SignIn: Component = () => {
               </div>
 
               <Button class='mt-4'>Sign in</Button>
-            </form>
+            </Form>
           </div>
         </div>
       </main>
