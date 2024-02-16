ebff61f97926114f78dc32f6dd9d71391b962909
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-02-16 12:17:53 +0100

    feat(frontend): add white logo variant & style header more in line with wireframe

--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -1,48 +1,54 @@
+import { useSignOutMutation } from '@/features/auth/api';
+import { A, useNavigate } from '@solidjs/router';
 import { Component } from 'solid-js';
 import Logo from '../logo/Logo';
-import { Input } from '../ui/input';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
-import { Button } from '../ui/button';
-import { useSignOutMutation } from '@/features/auth/api';
-import { useNavigate } from '@solidjs/router';
+import { Input } from '../ui/input';
 
 const Header: Component = () => {
   const signOut = useSignOutMutation();
   const navigate = useNavigate();
 
+  const handleSignOut = async () => {
+    try {
+      await signOut.mutateAsync();
+    } catch (error) {
+      // todo
+    }
+  };
+
   return (
-    <header>
-      <nav class='flex items-center justify-between gap-8 bg-gray-100 px-4'>
-        <Logo class='h-16 w-16' />
-        <Input class='max-w-screen-sm'></Input>
+    <header class='bg-brand-blue flex items-center justify-between gap-4 px-4 py-2'>
+      <nav class='flex flex-1 items-center justify-between'>
+        <A href='/'>
+          <Logo variant='white' class='h-8' />
+        </A>
+      </nav>
+
+      <section class='flex flex-1 items-center justify-center'>
+        <Input class='h-8 max-w-screen-sm' />
+      </section>
+
+      <section class='flex flex-1 flex-row-reverse'>
         <DropdownMenu>
-          <DropdownMenuTrigger as={Button}>Profile</DropdownMenuTrigger>
+          <DropdownMenuTrigger as={Avatar} class='h-8 w-8'>
+            <AvatarImage src='https://avatars.githubusercontent.com/u/47036430' />
+            <AvatarFallback>thedatasnok</AvatarFallback>
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem>
-              <span>Settings</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <span
-                onClick={async () => {
-                  try {
-                    await signOut.mutateAsync();
-                    navigate('/sign-in');
-                  } catch (error) {
-                    // todo
-                  }
-                }}
-              >
-                Sign out
-              </span>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem onClick={handleSignOut}>
+              Sign out
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      </nav>
+      </section>
     </header>
   );
 };
