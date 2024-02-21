f8dbfdfb8ed2a7e2e69d3c87e5aea4df31faf79d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-02-21 20:33:25 +0100

    feat(frontend): add custom linestring wkt parser that unprojects projected points

--- apps/frontend/src/components/map/MapRailingLayer.tsx
+++ apps/frontend/src/components/map/MapRailingLayer.tsx
@@ -1,32 +1,23 @@
 import Leaflet from 'leaflet';
 import { Component, onMount } from 'solid-js';
-import { useMap } from './MapRoot';
-import { parse } from 'wellknown';
+import { parse, useMap } from './MapRoot';
 
 const MapRailingLayer: Component = () => {
   const { map } = useMap();
 
-  var pointA = new Leaflet.LatLng(5, 5);
-  var pointB = new Leaflet.LatLng(-5, -5);
-  var pointList = [pointA, pointB];
-
   onMount(() => {
-    var firstpolyline = new Leaflet.Polyline(pointList, {
-      color: 'red',
-      weight: 2,
+    const linestring = parse(
+      'LINESTRING Z(204510.73 6876165.9 437.779, 204510.57 6876165.41 437.859, 204510.2 6876165.2 437.909, 204509.27 6876164.91 438.069, 204508.32 6876164.93 438.139, 204507.4 6876165.35 438.209, 204506.76 6876166.1 438.249, 204504.55 6876169.42 438.269, 204502.15 6876172.6 438.259, 204499.55 6876175.6 438.289, 204496.67 6876178.45 438.349, 204494.01 6876181.48 438.389, 204474.9 6876216.68 438.509, 204456.94 6876252.44 438.578, 204443.03 6876281.23 438.608, 204437.89 6876292.08 438.668, 204420.73 6876328.28 438.768, 204403.84 6876364.54 438.818, 204386.94 6876400.86 438.938, 204378.5 6876418.98 438.978, 204371.79 6876433.48 438.998, 204356.56 6876466.13 439.068, 204348.07 6876484.3 439.118, 204339.69 6876502.45 439.088, 204322.81 6876538.79 439.088, 204305.82 6876574.95 438.948, 204292.3 6876604.04 438.768, 204288.59 6876612.14 438.628)'
+    );
+
+    const line = Leaflet.polyline(linestring, {
+      color: 'blue',
+      weight: 8,
       opacity: 1,
       smoothFactor: 1,
-    });
+    }).addTo(map);
 
-    var linestring = parse('LINESTRING (6 2, 3 8, 4 -15)');
-    var geoJsonLayer = Leaflet.geoJson(linestring!);
-    geoJsonLayer.addTo(map);
-
-    firstpolyline.addTo(map);
-    map.fitBounds(firstpolyline.getBounds());
-
-    // console.log(firstpolyline);
-    // console.log(geoJsonLayer);
+    map.fitBounds(line.getBounds());
   });
 
   return null;
--- apps/frontend/src/components/map/MapRoot.tsx
+++ apps/frontend/src/components/map/MapRoot.tsx
@@ -9,11 +9,41 @@ import {
   onMount,
   useContext,
 } from 'solid-js';
-import proj4 from 'proj4leaflet';
+import 'proj4leaflet';
 
 export interface MapContextValue {
   map: Leaflet.Map;
 }
+
+// https://epsg.io/25833
+const CRS_PROJECTION = new Leaflet.Proj.CRS(
+  'EPSG:25833',
+  '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +vunits=m +no_defs +type=crs',
+  {
+    resolutions: [
+      21674.7100160867, 10837.3550080434, 5418.67750402168, 2709.33875201084,
+      1354.66937600542, 677.334688002709, 338.667344001355, 169.333672000677,
+      84.6668360003387, 42.3334180001693, 21.1667090000847, 10.5833545000423,
+      5.29167725002117, 2.64583862501058, 1.32291931250529, 0.661459656252646,
+      0.330729828126323, 0.165364914063161,
+    ],
+    origin: [-2500000, 9045984],
+  }
+);
+
+export const parse = (wkt: string) => {
+  const stripped = wkt
+    .replace('LINESTRING(', '')
+    .replace('LINESTRING Z(', '')
+    .replace(')', '');
+
+  const rawCoords = stripped.split(', ');
+
+  return rawCoords.map((pair) => {
+    const [x, y] = pair.split(' ').map((n) => parseFloat(n));
+    return CRS_PROJECTION.unproject(new Leaflet.Point(x, y));
+  });
+};
 
 const MapContext = createContext<MapContextValue>();
 
@@ -27,25 +57,14 @@ export const MapRoot: Component<{ children?: JSX.Element; class?: string }> = (
   onMount(() => {
     if (!container) return;
 
-    // const crs = new Leaflet.Proj.CRS(
-    //   'EPSG:25833',
-    //   '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
-    //   {
-    //     resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-    //     origin: [-2500000, 9045984],
-    //   }
-    // );
-
-    // https://epsg.io/25833
-
     const map = Leaflet.map(container, {
       center: [203174.5, 6876298.5],
       zoom: 7,
-      // crs: crs,
+      crs: CRS_PROJECTION,
     });
 
     // better way to do this is to change maxZoom to length of resolution list from crs i think
-    const layer = new TileLayer(
+    const layer = new Leaflet.TileLayer(
       'https://services.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}',
       { maxZoom: 14, minZoom: 0 }
     );
