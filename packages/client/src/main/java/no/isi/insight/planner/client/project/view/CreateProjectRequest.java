894a636f361bd3fadce56397195790f8100c049e
GBFur <zifergames@gmail.com>
2024-02-23 13:21:41 +0100

    feat(backend): add validation constraints to create project request

--- packages/client/src/main/java/no/isi/insight/planner/client/project/view/CreateProjectRequest.java
+++ packages/client/src/main/java/no/isi/insight/planner/client/project/view/CreateProjectRequest.java
@@ -2,7 +2,14 @@ package no.isi.insight.planner.client.project.view;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import no.isi.insight.planner.client.annotation.GenerateTypeScript;
 
 @GenerateTypeScript
-public record CreateProjectRequest(String name, String referenceCode, LocalDate startsAt, LocalDate endsAt) {}
+public record CreateProjectRequest(
+  @NotBlank String name,
+  @NotBlank String referenceCode,
+  @NotNull LocalDate startsAt,
+  @NotNull LocalDate endsAt
+) {}
