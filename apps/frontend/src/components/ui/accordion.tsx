0ba4ea7225628f9a98c06822730c23d4b9aadff4
Dawid Staniszewski <dstaniszewski00@gmail.com>
2024-02-25 23:33:46 +0100

    fix(frontend): B24-27 remove border from accordion + formatting

--- apps/frontend/src/components/ui/accordion.tsx
+++ apps/frontend/src/components/ui/accordion.tsx
@@ -1,49 +1,55 @@
-import type { Component } from "solid-js"
-import { splitProps } from "solid-js"
+import type { Component } from 'solid-js';
+import { splitProps } from 'solid-js';
 
-import { Accordion as AccordionPrimitive } from "@kobalte/core"
-import { TbChevronDown } from "solid-icons/tb"
+import { Accordion as AccordionPrimitive } from '@kobalte/core';
+import { TbChevronDown } from 'solid-icons/tb';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-const Accordion = AccordionPrimitive.Root
+const Accordion = AccordionPrimitive.Root;
 
-const AccordionItem: Component<AccordionPrimitive.AccordionItemProps> = (props) => {
-  const [, rest] = splitProps(props, ["class"])
-  return <AccordionPrimitive.Item class={cn("border-b", props.class)} {...rest} />
-}
+const AccordionItem: Component<AccordionPrimitive.AccordionItemProps> = (
+  props
+) => {
+  const [, rest] = splitProps(props, ['class']);
+  return <AccordionPrimitive.Item class={cn('', props.class)} {...rest} />;
+};
 
-const AccordionTrigger: Component<AccordionPrimitive.AccordionTriggerProps> = (props) => {
-  const [, rest] = splitProps(props, ["class", "children"])
+const AccordionTrigger: Component<AccordionPrimitive.AccordionTriggerProps> = (
+  props
+) => {
+  const [, rest] = splitProps(props, ['class', 'children']);
   return (
-    <AccordionPrimitive.Header class="flex">
+    <AccordionPrimitive.Header class='flex'>
       <AccordionPrimitive.Trigger
         class={cn(
-          "flex flex-1 items-center justify-between bg-gray-100 px-2 py-1 text-xl font-semibold transition-all hover:underline [&[data-expanded]>svg]:rotate-180",
+          'flex flex-1 items-center justify-between bg-gray-100 px-2 py-1 text-xl font-semibold transition-all hover:underline [&[data-expanded]>svg]:rotate-180',
           props.class
         )}
         {...rest}
       >
         {props.children}
-        <TbChevronDown class="size-4 shrink-0 transition-transform duration-200" />
+        <TbChevronDown class='size-4 shrink-0 transition-transform duration-200' />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  )
-}
+  );
+};
 
-const AccordionContent: Component<AccordionPrimitive.AccordionContentProps> = (props) => {
-  const [, rest] = splitProps(props, ["class", "children"])
+const AccordionContent: Component<AccordionPrimitive.AccordionContentProps> = (
+  props
+) => {
+  const [, rest] = splitProps(props, ['class', 'children']);
   return (
     <AccordionPrimitive.Content
       class={cn(
-        "animate-accordion-up overflow-hidden text-sm transition-all data-[expanded]:animate-accordion-down",
+        'animate-accordion-up data-[expanded]:animate-accordion-down overflow-hidden text-sm transition-all',
         props.class
       )}
       {...rest}
     >
       {props.children}
     </AccordionPrimitive.Content>
-  )
-}
+  );
+};
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
