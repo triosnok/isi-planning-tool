67ca8e7c6370d7f5d093231f3c74cdc0e3bb23e5
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-02-29 12:37:24 +0100

    build(client): map dates in generated types to string
    since we don't have a JSON processor in place we can't assume that the dates are parsed into actual Date objects

--- packages/client/src/main/typescript/index.ts
+++ packages/client/src/main/typescript/index.ts
@@ -17,4 +17,60 @@ export interface UserProfile {
     role: UserRole;
 }
 
+export interface CreateProjectPlanRequest {
+    importUrl: string;
+    startsAt: DateAsString;
+    endsAt: DateAsString;
+}
+
+export interface CreateProjectRequest {
+    name: string;
+    referenceCode: string;
+    startsAt: DateAsString;
+    endsAt: DateAsString;
+}
+
+export interface ProjectDetails {
+    id: string;
+    name: string;
+    referenceCode: string;
+    startsAt: DateAsString;
+    endsAt: DateAsString;
+}
+
+export interface ProjectPlanDetails {
+    id: string;
+    startsAt: DateAsString;
+    endsAt: DateAsString;
+    vehicleModel: string;
+    registrationNumber: string;
+    activeTrips: number;
+    railings: number;
+    meters: number;
+}
+
+export interface RoadRailing {
+    wkt: string;
+    srid: number;
+}
+
+export interface CreateVehicleRequest {
+    imageUrl: string;
+    registrationNumber: string;
+    camera: boolean;
+    description: string;
+    gnssId: string;
+}
+
+export interface VehicleDetails {
+    id: string;
+    imageUrl: string;
+    registrationNumber: string;
+    camera: boolean;
+    description: string;
+    gnssId: string;
+}
+
+export type DateAsString = string;
+
 export type UserRole = "DRIVER" | "PLANNER";
