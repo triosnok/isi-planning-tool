bddf8298d6e95ac7c8584dd193304ec8eece8e24
GBFur <zifergames@gmail.com>
2024-02-29 16:03:27 +0100

    feat(backend): B24-89 implement functionality for marking a vehicle as inactive

--- packages/client/src/main/java/no/isi/insight/planner/client/vehicle/VehicleRestService.java
+++ packages/client/src/main/java/no/isi/insight/planner/client/vehicle/VehicleRestService.java
@@ -12,28 +12,29 @@ import org.springframework.web.service.annotation.PostExchange;
 import org.springframework.web.service.annotation.PutExchange;
 
 import no.isi.insight.planner.client.vehicle.view.CreateVehicleRequest;
+import no.isi.insight.planner.client.vehicle.view.UpdateVehicleRequest;
 import no.isi.insight.planner.client.vehicle.view.VehicleDetails;
 
 @HttpExchange("/api/v1/vehicles")
 public interface VehicleRestService {
 
-  @PostExchange
-  ResponseEntity<VehicleDetails> createVehicle(
-      @Validated @RequestBody CreateVehicleRequest request
-  );
+    @PostExchange
+    ResponseEntity<VehicleDetails> createVehicle(
+            @Validated @RequestBody CreateVehicleRequest request
+    );
 
-  @PutExchange("/{id}")
-  ResponseEntity<VehicleDetails> updateVehicle(
-      @PathVariable UUID id,
-      @Validated @RequestBody CreateVehicleRequest request
-  );
+    @PutExchange("/{id}")
+    ResponseEntity<VehicleDetails> updateVehicle(
+            @PathVariable UUID id,
+            @Validated @RequestBody UpdateVehicleRequest request
+    );
 
-  @GetExchange("/{id}")
-  ResponseEntity<VehicleDetails> findVehicle(
-      @PathVariable UUID id
-  );
+    @GetExchange("/{id}")
+    ResponseEntity<VehicleDetails> findVehicle(
+            @PathVariable UUID id
+    );
 
-  @GetExchange
-  ResponseEntity<VehicleDetails[]> findAllVehicles();
+    @GetExchange
+    ResponseEntity<VehicleDetails[]> findAllVehicles();
 
 }
