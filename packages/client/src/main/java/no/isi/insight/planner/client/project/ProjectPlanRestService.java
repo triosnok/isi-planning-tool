b15771f9ef5015a3165d66264ee5623b842153f7
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-01 11:04:11 +0100

    feat: add endpoint for listing project plans for a project

--- packages/client/src/main/java/no/isi/insight/planner/client/project/ProjectPlanRestService.java
+++ packages/client/src/main/java/no/isi/insight/planner/client/project/ProjectPlanRestService.java
@@ -1,10 +1,12 @@
 package no.isi.insight.planner.client.project;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 import org.springframework.web.service.annotation.PutExchange;
@@ -15,6 +17,17 @@ import no.isi.insight.planner.client.project.view.UpdateProjectPlanRequest;
 
 @HttpExchange("/api/v1/projects/{projectId}/plans")
 public interface ProjectPlanRestService {
+
+  @GetExchange
+  ResponseEntity<List<ProjectPlanDetails>> getPlans(
+      @PathVariable UUID projectId
+  );
+
+  @GetExchange("/{planId}")
+  ResponseEntity<ProjectPlanDetails> getPlan(
+      @PathVariable UUID projectId,
+      @PathVariable UUID planId
+  );
 
   @PostExchange
   ResponseEntity<ProjectPlanDetails> createPlan(
