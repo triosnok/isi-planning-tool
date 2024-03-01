0741d729b7cc8fc5d9ada679aee4edc6a17d280b
dawsta27 <dstaniszewski00@gmail.com>
2024-03-01 15:14:12 +0100

    fix(frontend): make progress bar component use class prop

--- apps/frontend/src/components/ui/progress.tsx
+++ apps/frontend/src/components/ui/progress.tsx
@@ -4,13 +4,19 @@ import { splitProps } from 'solid-js';
 import { Progress as ProgressPrimitive } from '@kobalte/core';
 
 import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
 
 const Progress: Component<ProgressPrimitive.ProgressRootProps> = (props) => {
-  const [, rest] = splitProps(props, ['children']);
+  const [, rest] = splitProps(props, ['children', 'class']);
   return (
     <ProgressPrimitive.Root {...rest}>
       {props.children}
-      <ProgressPrimitive.Track class='relative h-4 w-full overflow-hidden bg-gray-200'>
+      <ProgressPrimitive.Track
+        class={cn(
+          'relative h-4 w-full overflow-hidden bg-gray-200',
+          props.class
+        )}
+      >
         <ProgressPrimitive.Fill class='bg-primary h-full w-[var(--kb-progress-fill-width)] flex-1 transition-all' />
       </ProgressPrimitive.Track>
     </ProgressPrimitive.Root>
