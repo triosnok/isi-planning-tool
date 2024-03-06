c2f29b228c7d6c9e03e140867cea0b0dda481492
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-06 22:09:31 +0100

    fix(backend): B24-108 use JTS point instead of spring data point

--- apps/backend/src/main/java/no/isi/insight/planning/model/TripNote.java
+++ apps/backend/src/main/java/no/isi/insight/planning/model/TripNote.java
@@ -2,7 +2,7 @@ package no.isi.insight.planning.model;
 
 import java.util.UUID;
 
-import org.springframework.data.geo.Point;
+import org.locationtech.jts.geom.Point;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
