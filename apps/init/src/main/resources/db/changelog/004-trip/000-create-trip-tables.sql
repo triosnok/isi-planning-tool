ceb24d21181d9724256692ef3b512f1b500c4a80
GBFur <zifergames@gmail.com>
2024-03-06 18:19:57 +0100

    feat(backend): B24-108 add migrations & model for trip note

--- apps/init/src/main/resources/db/changelog/004-trip/000-create-trip-tables.sql
+++ apps/init/src/main/resources/db/changelog/004-trip/000-create-trip-tables.sql
@@ -17,3 +17,18 @@ CREATE TABLE trip (
   FOREIGN KEY (fk_driver_user_id) REFERENCES user_account (user_account_id),
   FOREIGN KEY (fk_vehicle_id) REFERENCES vehicle (vehicle_id)
 );
+
+CREATE TABLE trip_note (
+  trip_note_id UUID NOT NULL DEFAULT gen_random_uuid(),
+  fk_trip_id UUID NOT NULL,
+  note TEXT,
+  position POINT,
+  created_at TIMESTAMP DEFAULT NOW(),
+  fk_created_by_user_id UUID,
+  updated_at TIMESTAMP,
+  fk_updated_by_user_id UUID,
+
+  PRIMARY KEY (trip_note_id),
+
+  FOREIGN KEY (fk_trip_id) REFERENCES trip (trip_id)
+);
