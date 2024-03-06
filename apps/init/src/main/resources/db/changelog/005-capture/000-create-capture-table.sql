e8981d6575bad6003c9d649e94e5aa2140a63398
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-06 23:55:40 +0100

    fix(init): B24-107 add missing migration file
    oops...

---
+++ apps/init/src/main/resources/db/changelog/005-capture/000-create-capture-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE trip_railing_capture (
+  trip_railing_capture_id UUID NOT NULL DEFAULT gen_random_uuid(),
+  fk_trip_id UUID NOT NULL,
+  fk_road_railing_id UUID NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  position POINT,
+  image_urls JSON,
+
+  PRIMARY KEY (trip_railing_capture_id),
+  
+  FOREIGN KEY (fk_trip_id) REFERENCES trip (trip_id),
+  FOREIGN KEY (fk_road_railing_id) REFERENCES road_railing (road_railing_id)
+);
