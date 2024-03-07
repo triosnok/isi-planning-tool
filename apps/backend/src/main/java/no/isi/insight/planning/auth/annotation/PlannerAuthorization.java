20f59bfc61d77d22a06169574a78e483f9f98d5e
GBFur <zifergames@gmail.com>
2024-03-07 13:58:24 +0100

    fix(backend): update autherization annotation

--- apps/backend/src/main/java/no/isi/insight/planning/auth/annotation/PlannerAuthorization.java
+++ apps/backend/src/main/java/no/isi/insight/planning/auth/annotation/PlannerAuthorization.java
@@ -13,5 +13,5 @@ import org.springframework.security.access.prepost.PreAuthorize;
     ElementType.METHOD, ElementType.TYPE
 })
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasRole('PLANNER')")
+@PreAuthorize("hasAuthority('PLANNER')")
 public @interface PlannerAuthorization {}
