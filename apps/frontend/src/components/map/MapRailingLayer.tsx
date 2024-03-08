1264a425bc31dc9d26522dfb8faa8b0f0eff2977
dawsta27 <dstaniszewski00@gmail.com>
2024-03-08 12:39:47 +0100

    feat(frontend): B24-112 add direction indicators to railings

--- apps/frontend/src/components/map/MapRailingLayer.tsx
+++ apps/frontend/src/components/map/MapRailingLayer.tsx
@@ -2,6 +2,7 @@ import { RoadRailing } from '@isi-insight/client';
 import Leaflet from 'leaflet';
 import { Component, createEffect, onCleanup } from 'solid-js';
 import { parse, useMap } from './MapRoot';
+import 'leaflet-textpath';
 
 export interface MapRailingLayerProps {
   railings?: RoadRailing[];
@@ -15,13 +16,20 @@ const MapRailingLayer: Component<MapRailingLayerProps> = (props) => {
       const ls = parse(railing.wkt);
       return Leaflet.polyline(ls, {
         color: 'blue',
-        weight: 8,
+        weight: 4,
         opacity: 1,
         smoothFactor: 1,
       });
     });
 
     const lg = Leaflet.layerGroup(polylines);
+
+    lg.setText('  ►  ', {
+      repeat: true,
+      center: true,
+      offset: 10,
+      attributes: { fill: 'blue' },
+    });
 
     lg.addTo(map);
     onCleanup(() => lg.removeFrom(map));
