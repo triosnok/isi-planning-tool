d2f467eca5e1a2918f104db98db23874ab42e522
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-18 12:22:45 +0100

    chore(backend): remove unnecessary logging

--- apps/backend/src/main/java/no/isi/insight/planning/capture/service/CaptureLogProcessor.java
+++ apps/backend/src/main/java/no/isi/insight/planning/capture/service/CaptureLogProcessor.java
@@ -187,7 +187,6 @@ public class CaptureLogProcessor {
       } else if (deltaMs > this.maxDeltaMs) {
         return CameraLogMatchResult.skip(Optional.of(nextEntry));
       } else {
-        log.info("iterating to next delta {}", deltaMs);
         nextEntry = iterator.hasNext() ? iterator.next() : null;
       }
     }
