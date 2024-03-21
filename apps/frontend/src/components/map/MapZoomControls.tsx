3d8aad504d60762b0af063f28543ad9f22f8ab1d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-21 10:09:27 +0100

    feat(frontend): B24-135 style map zoom controls & handle button disabled state

--- apps/frontend/src/components/map/MapZoomControls.tsx
+++ apps/frontend/src/components/map/MapZoomControls.tsx
@@ -1,13 +1,57 @@
-import { Component } from 'solid-js';
+import { cn } from '@/lib/utils';
+import { IconMinus, IconPlus } from '@tabler/icons-solidjs';
+import { Component, createSignal, onCleanup, onMount } from 'solid-js';
 import { useMap } from './MapRoot';
 
-const MapZoomControls: Component = () => {
+export interface MapZoomControlsProps {
+  class?: string;
+}
+
+const MapZoomControls: Component<MapZoomControlsProps> = (props) => {
   const ctx = useMap();
+  const [canZoomIn, setCanZoomIn] = createSignal(
+    ctx.map.getZoom() < ctx.map.getMaxZoom()
+  );
+
+  const [canZoomOut, setCanZoomOut] = createSignal(
+    ctx.map.getZoom() > ctx.map.getMinZoom()
+  );
+
+  const zoomIn = () => ctx.map.zoomIn(1);
+  const zoomOut = () => ctx.map.zoomOut(1);
+
+  onMount(() => {
+    const onZoom = () => {
+      setCanZoomIn(ctx.map.getZoom() < ctx.map.getMaxZoom());
+      setCanZoomOut(ctx.map.getZoom() > ctx.map.getMinZoom());
+    };
+
+    ctx.map.addEventListener('zoom', onZoom);
+
+    onCleanup(() => {
+      ctx.map.removeEventListener('zoom', onZoom);
+    });
+  });
 
   return (
-    <div>
-      <button onClick={() => ctx.map.zoomIn(1)}>+</button>
-      <button onClick={() => ctx.map.zoomOut(1)}>-</button>
+    <div class={cn('flex flex-col divide-y rounded-md shadow', props.class)}>
+      <button
+        disabled={!canZoomIn()}
+        class='rounded-t-md border border-gray-300 bg-gray-50 p-1 transition-colors hover:bg-gray-200 focus:bg-gray-200 disabled:text-gray-400'
+        onClick={zoomIn}
+      >
+        <IconPlus class='h-6 w-6' />
+        <span class='sr-only'>Zoom in</span>
+      </button>
+
+      <button
+        disabled={!canZoomOut()}
+        class='-mt-px rounded-b-md border border-gray-300 bg-gray-50 p-1 transition-colors hover:bg-gray-200 focus:bg-gray-200 disabled:text-gray-400'
+        onClick={zoomOut}
+      >
+        <IconMinus class='h-6 w-6' />
+        <span class='sr-only'>Zoom out</span>
+      </button>
     </div>
   );
 };
