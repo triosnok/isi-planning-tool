68de51cd81c3cd9d7f5806748606eb45bce9ccbe
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-26 14:58:11 +0100

    fix(backend): properly configure minio container & client for running in dev

--- apps/backend/src/test/java/no/isi/insight/planning/PlanningApplicationDevelopment.java
+++ apps/backend/src/test/java/no/isi/insight/planning/PlanningApplicationDevelopment.java
@@ -1,13 +1,16 @@
 package no.isi.insight.planning;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import io.minio.MinioClient;
 import no.isi.insight.planning.auth.service.UserAccountService;
 import no.isi.insight.planning.model.UserAccountRole;
 import no.isi.insight.planning.repository.UserAccountJpaRepository;
@@ -21,12 +24,21 @@ public class PlanningApplicationDevelopment {
   PostgreSQLContainer<?> postgresContainer() {
     return new PostgreSQLContainer<>(TestcontainersConfiguration.POSTGIS_IMAGE_NAME);
   }
-  
-  @Bean
-  @RestartScope
-  @ServiceConnection
+
+  @Bean(initMethod = "start", destroyMethod = "stop")
   MinIOContainer minioContainer() {
     return new MinIOContainer("minio/minio:RELEASE.2024-03-21T23-13-43Z");
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "no.isi.insight.planning.integration.minio.url", matchIfMissing = true)
+  MinioClient minioClient(
+      MinIOContainer container
+  ) {
+    return MinioClient.builder()
+      .endpoint(container.getS3URL())
+      .credentials(container.getUserName(), container.getPassword())
+      .build();
   }
 
   public static void main(
