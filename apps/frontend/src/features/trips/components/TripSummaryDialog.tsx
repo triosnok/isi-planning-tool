584e3a0be19c2560d2a3c2093185b335eff20a6f
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-28 16:13:39 +0100

    feat(frontend): B24-165 add utility for analyzing the amount of images captured & minor changes to dialog

--- apps/frontend/src/features/trips/components/TripSummaryDialog.tsx
+++ apps/frontend/src/features/trips/components/TripSummaryDialog.tsx
@@ -6,13 +6,22 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { useTranslations } from '@/features/i18n';
+import { CameraPosition } from '@isi-insight/client';
 import { useNavigate } from '@solidjs/router';
-import { IconCheck, IconCircleCheckFilled } from '@tabler/icons-solidjs';
+import {
+  IconAlertCircleFilled,
+  IconCheck,
+  IconCircleCheckFilled,
+  IconCircleXFilled,
+  IconExclamationMark,
+  IconX,
+} from '@tabler/icons-solidjs';
 import { cva } from 'class-variance-authority';
 import dayjs from 'dayjs';
-import { Component } from 'solid-js';
+import { Component, For, Match, Switch } from 'solid-js';
 import { z } from 'zod';
 import { useTripMutation } from '../api';
+import { ImageAnalysis, ImageStatus, getImageAnalysis } from '../utils';
 
 export interface TripSummaryDialogProps {
   open: boolean;
@@ -51,7 +60,13 @@ const TripSummaryDialog: Component<TripSummaryDialogProps> = (props) => {
           <DialogTitle>{t('TRIPS.TRIP_SUMMARY.TITLE')}</DialogTitle>
         </DialogHeader>
 
-        <ImageSummary />
+        <ImageSummary
+          analysis={getImageAnalysis({
+            LEFT: 24000,
+            RIGHT: 23993,
+            TOP: 12111,
+          })}
+        />
 
         <Button
           variant='destructive'
@@ -69,81 +84,110 @@ const TripSummaryDialog: Component<TripSummaryDialogProps> = (props) => {
 };
 
 interface ImageSummaryProps {
-  //TODO: Add necessary props
+  analysis: ImageAnalysis;
 }
 
-enum TripStatus {
-  OK,
-  WITHIN_TOLERANCE,
-  OUT_OF_TOLERANCE,
+const tripSummaryVariants = cva(
+  'relative overflow-hidden rounded-lg border p-2 justify-between',
+  {
+    variants: {
+      status: {
+        [ImageStatus.OK]:
+          'border-success-600 bg-success-50 text-success-950 dark:bg-success-950/25 dark:text-success-50',
+        [ImageStatus.OUT_OF_TOLERANCE]:
+          'border-error-600 bg-error-50 dark:bg-error-950/25 dark:border-error-800',
+        [ImageStatus.WITHIN_TOLERANCE]:
+          'border-warning-600 bg-warning-50 dark:bg-warning-950/25 dark:border-warning-800',
+      },
+    },
+    defaultVariants: {
+      status: ImageStatus.OK,
+    },
+  }
+);
+
+interface ImagePositionStatusProps {
+  position: CameraPosition;
+  count: number;
+  status: ImageStatus;
 }
 
-const ImageSummary: Component<ImageSummaryProps> = () => {
-  const { t } = useTranslations();
-
-  //TODO: Add necessary logic to determine the status of the trip
-
-  const tripSummaryVariants = cva(
-    'relative transition-colors hover:cursor-pointer overflow-hidden rounded-lg border p-2 justify-between',
-    {
-      variants: {
-        status: {
-          [TripStatus.OK]:
-            'border-success-600 hover:bg-success-100 dark:hover:bg-success-900 bg-success-50 text-success-950 dark:bg-success-950 dark:text-success-50',
-          [TripStatus.OUT_OF_TOLERANCE]:
-            'border-error-600 hover:bg-error-100 dark:hover:bg-error-900 bg-error-50 dark:bg-error-950 dark:border-error-800',
-          [TripStatus.WITHIN_TOLERANCE]:
-            'border-warning-600 hover:bg-warning-100 dark:hover:bg-warning-900 bg-warning-50 dark:bg-warning-950 dark:border-warning-800',
-        },
-      },
-      defaultVariants: {
-        status: TripStatus.OK,
-      },
-    }
-  );
+const ImagePositionStatus: Component<ImagePositionStatusProps> = (props) => {
+  const { t, n } = useTranslations();
 
   return (
-    <div class={tripSummaryVariants({ status: TripStatus.OK })}>
+    <div class='flex flex-row items-center justify-between'>
+      <p class='text-sm'>{t(`TRIPS.TRIP_SUMMARY.${props.position}`)}</p>
+
+      <div class='flex items-center gap-2'>
+        <p class='text-sm'>{n(props.count)}</p>
+
+        <Switch>
+          <Match when={props.status === ImageStatus.OK}>
+            <IconCheck class='text-success-600 size-5' />
+          </Match>
+          <Match when={props.status === ImageStatus.OUT_OF_TOLERANCE}>
+            <IconX class='text-error-600 size-5' />
+          </Match>
+          <Match when={props.status === ImageStatus.WITHIN_TOLERANCE}>
+            <IconExclamationMark class='text-warning-600 size-5' />
+          </Match>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+const ImageSummary: Component<ImageSummaryProps> = (props) => {
+  const { t } = useTranslations();
+
+  return (
+    <div class={tripSummaryVariants({ status: props.analysis.overall })}>
       <div class='flex flex-row justify-between pb-2'>
         <p class='max-w-full self-center truncate font-medium'>
           {t('TRIPS.TRIP_SUMMARY.AMOUNT_OF_IMAGES_TAKEN')}
         </p>
-        <IconCircleCheckFilled class='text-success-600 size-5' />
-      </div>
-      <div class='flex flex-row items-center justify-between'>
-        <div>
-          <p class='text-sm'>{t('TRIPS.TRIP_SUMMARY.LEFT')}</p>
-        </div>
-        <div class='flex items-center'>
-          <p class='mr-2 text-sm'>24000</p>
-          <IconCheck class='text-success-600 size-5' />
-        </div>
+
+        <Switch>
+          <Match when={props.analysis.overall === ImageStatus.OK}>
+            <IconCircleCheckFilled class='text-success-600 size-5' />
+          </Match>
+          <Match when={props.analysis.overall === ImageStatus.OUT_OF_TOLERANCE}>
+            <IconCircleXFilled class='text-error-600 size-5' />
+          </Match>
+          <Match when={props.analysis.overall === ImageStatus.WITHIN_TOLERANCE}>
+            <IconAlertCircleFilled class='text-warning-600 size-5' />
+          </Match>
+        </Switch>
       </div>
 
-      <div class='flex flex-row items-center justify-between'>
-        <div>
-          <p class='text-sm'>{t('TRIPS.TRIP_SUMMARY.RIGHT')}</p>
-        </div>
-        <div class='flex items-center'>
-          <p class='mr-2 text-sm'>24000</p>
-          <IconCheck class='text-success-600 size-5' />
-        </div>
-      </div>
+      <ImagePositionStatus
+        position='LEFT'
+        count={props.analysis.positions.LEFT.count}
+        status={props.analysis.positions.LEFT.status}
+      />
 
-      <div class='flex flex-row items-center justify-between'>
-        <div>
-          <p class='text-sm'>{t('TRIPS.TRIP_SUMMARY.TOP')}</p>
-        </div>
-        <div class='flex items-center'>
-          <p class='mr-2 text-sm'>12000</p>
-          <IconCheck class='text-success-600 size-5' />
-        </div>
-      </div>
+      <ImagePositionStatus
+        position='RIGHT'
+        count={props.analysis.positions.RIGHT.count}
+        status={props.analysis.positions.RIGHT.status}
+      />
 
-      {/*//TODO: Add appropriate error message */}
-      {/* <Show when={false}>
-        <p class='text-error-600 pt-2'>Images missing from top camera</p>
-      </Show> */}
+      <ImagePositionStatus
+        position='TOP'
+        count={props.analysis.positions.TOP.count}
+        status={props.analysis.positions.TOP.status}
+      />
+
+      <ul class='text-error-500 mt-2 list-inside list-disc text-sm'>
+        <For each={props.analysis.remarks}>
+          {(remark) => (
+            <li>
+              <span class='-ml-2'>{t(`TRIPS.TRIP_SUMMARY.REMARKS.${remark}`)}</span>
+            </li>
+          )}
+        </For>
+      </ul>
     </div>
   );
 };
