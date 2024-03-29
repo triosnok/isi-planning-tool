eddcedee15e80461bf75002c1e58fd9e877c64a9
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-03-29 13:32:13 +0100

    fix(backend): fix broken column references & add reverse mapping to segments from railings

--- apps/backend/src/main/java/no/isi/insight/planning/model/RoadRailing.java
+++ apps/backend/src/main/java/no/isi/insight/planning/model/RoadRailing.java
@@ -1,10 +1,14 @@
 package no.isi.insight.planning.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.locationtech.jts.geom.LineString;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,6 +36,9 @@ public class RoadRailing {
 
   @Column(name = "own_geometry")
   private boolean ownGeometry;
+
+  @OneToMany(mappedBy = "railing")
+  private List<RoadSegment> roadSegments = new ArrayList<>();
 
   public RoadRailing(
       Long id,
