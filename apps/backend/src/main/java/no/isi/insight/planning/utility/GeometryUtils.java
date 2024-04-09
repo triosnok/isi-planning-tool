d21de60251ac291c9379a040e633fbe5424ee859
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-04-09 15:45:20 +0200

    fix(backend): include Z coordinates when converting geometries to WKT
    closes B24-218

--- apps/backend/src/main/java/no/isi/insight/planning/utility/GeometryUtils.java
+++ apps/backend/src/main/java/no/isi/insight/planning/utility/GeometryUtils.java
@@ -1,8 +1,12 @@
 package no.isi.insight.planning.utility;
 
+import org.locationtech.jts.io.WKTWriter;
+
 import no.isi.insight.planning.client.geometry.Geometry;
 
 public class GeometryUtils {
+
+  private static final WKTWriter WKT_WRITER = new WKTWriter(3);
 
   public static Geometry toClientGeometry(
       org.locationtech.jts.geom.Geometry geom
@@ -11,9 +15,21 @@ public class GeometryUtils {
       return null;
     }
     return new Geometry(
-      geom.toText(),
+      GeometryUtils.toWkt(geom),
       geom.getSRID()
     );
+  }
+
+  public static String toWkt(
+      org.locationtech.jts.geom.Geometry geom
+  ) {
+    var coord = geom.getCoordinate();
+
+    if (Double.isNaN(coord.getZ())) {
+      return geom.toText();
+    }
+
+    return WKT_WRITER.write(geom);
   }
 
 }
