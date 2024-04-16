e0230dce2378dc24d5392f70da5a60b0281bee09
dawsta27 <dstaniszewski00@gmail.com>
2024-04-16 13:20:47 +0200

    feat(frontend): improve password reset ui
    closes B24-225

--- apps/frontend/src/features/auth/routes/ForgotPassword.tsx
+++ apps/frontend/src/features/auth/routes/ForgotPassword.tsx
@@ -8,6 +8,7 @@ import { Component, Match, Switch, createSignal } from 'solid-js';
 import { createStore } from 'solid-js/store';
 import { z } from 'zod';
 import { useForgotPasswordMutation } from '../api';
+import BackLink from '@/components/navigation/BackLink';
 
 interface ForgotPasswordState {
   step: Step;
@@ -31,7 +32,7 @@ const ForgotPassword: Component = () => {
 
   return (
     <div class='flex h-svh w-svw items-center justify-center'>
-      <main class='m-4 flex w-full max-w-md flex-col rounded-md bg-gray-900 p-4'>
+      <main class='m-4 flex w-full max-w-md flex-col rounded-lg border border-gray-100 p-4 shadow-lg dark:border-gray-800 dark:bg-gray-900'>
         <Switch>
           <Match when={store.step === Step.SEND_CODE}>
             <SendCodeStep
@@ -49,6 +50,9 @@ const ForgotPassword: Component = () => {
               onSuccess={(resetCode) => {
                 setStore('step', Step.RESET_PASSWORD);
                 setStore('resetCode', resetCode);
+              }}
+              onNotYou={() => {
+                setStore('step', Step.SEND_CODE);
               }}
             />
           </Match>
@@ -70,6 +74,7 @@ interface StepProps<T> {
   email: string;
   resetCode?: string;
   onSuccess: (v: T) => void;
+  onNotYou?: () => void;
 }
 
 const SendCodeSchema = z.object({ email: z.string().email() });
@@ -87,17 +92,32 @@ const SendCodeStep: Component<StepProps<string>> = (props) => {
   };
 
   return (
-    <Form onSubmit={handleSubmit} class='flex flex-col gap-2'>
+    <Form onSubmit={handleSubmit} class='flex flex-col gap-2 py-2'>
+      <BackLink />
+      <div class='mb-4'>
+        <h1 class='text-xl font-semibold'>
+          {t('AUTHENTICATION.RESET_PASSWORD.RESET_YOUR_PASSWORD')}
+        </h1>
+        <h2 class='text-sm text-gray-600 dark:text-gray-400'>
+          {t('AUTHENTICATION.RESET_PASSWORD.PLEASE_ENTER_EMAIL')}
+        </h2>
+      </div>
       <Field name='email'>
         {(field, props) => (
           <>
             <Label>{t('USERS.FORM.EMAIL')}</Label>
-            <Input {...props} value={field.value} />
+            <Input
+              {...props}
+              value={field.value}
+              placeholder={t('USERS.FORM.EMAIL')}
+            />
           </>
         )}
       </Field>
 
-      <Button type='submit'>Send code</Button>
+      <Button type='submit'>
+        {t('AUTHENTICATION.RESET_PASSWORD.SEND_CODE')}
+      </Button>
     </Form>
   );
 };
@@ -122,15 +142,37 @@ const ConfirmCodeStep: Component<StepProps<string>> = (props) => {
   };
 
   return (
-    <>
-      <div class='flex flex-col gap-2'>
-        <Label>{t('AUTHENTICATION.RESET_PASSWORD.CONFIRMATION_CODE')}</Label>
-
-        <Input value={code()} onChange={(e) => setCode(e.target.value)} />
-
-        <Button onClick={handleSubmit}>Confirm code</Button>
+    <div class='flex flex-col gap-2 py-2'>
+      <BackLink />
+      <div class='mb-4'>
+        <h1 class='text-xl font-semibold'>
+          {t('AUTHENTICATION.RESET_PASSWORD.ENTER_CONFIRMATION_CODE')}
+        </h1>
+        <h2 class='text-sm text-gray-600 dark:text-gray-400'>
+          {t('AUTHENTICATION.RESET_PASSWORD.PLEASE_ENTER_CONFIRMATION_CODE')}{' '}
+          <span class='font-semibold text-gray-950 dark:text-gray-50'>
+            {props.email}
+          </span>
+        </h2>
+        <span
+          onClick={props.onNotYou}
+          class='mt-2 cursor-pointer text-xs text-gray-500 hover:underline'
+        >
+          {t('AUTHENTICATION.RESET_PASSWORD.NOT_YOU')}
+        </span>
       </div>
-    </>
+
+      <Label>{t('AUTHENTICATION.RESET_PASSWORD.CONFIRMATION_CODE')}</Label>
+      <Input
+        value={code()}
+        placeholder={t('AUTHENTICATION.RESET_PASSWORD.CONFIRMATION_CODE')}
+        onChange={(e) => setCode(e.target.value)}
+      />
+
+      <Button onClick={handleSubmit}>
+        {t('AUTHENTICATION.RESET_PASSWORD.CONFIRM_CODE')}
+      </Button>
+    </div>
   );
 };
 
@@ -166,31 +208,51 @@ const ResetPasswordStep: Component<StepProps<void>> = (props) => {
   };
 
   return (
-    <>
-      <Form onSubmit={handleSubmit} class='flex flex-col gap-2'>
-        <Field name='password'>
-          {(field, props) => (
-            <>
-              <Label>{t('USERS.FORM.PASSWORD')}</Label>
-              <Input {...props} type='password' value={field.value} />
-            </>
-          )}
-        </Field>
+    <Form onSubmit={handleSubmit} class='flex flex-col gap-2 py-2'>
+      <BackLink />
+      <div class='mb-4'>
+        <h1 class='text-xl font-semibold'>
+          {t('AUTHENTICATION.RESET_PASSWORD.ENTER_NEW_PASSWORD')}
+        </h1>
+        <h2 class='text-sm text-gray-600 dark:text-gray-400'>
+          {t('AUTHENTICATION.RESET_PASSWORD.PLEASE_ENTER_NEW_PASSWORD')}
+        </h2>
+      </div>
 
-        <Field name='passwordConfirmation'>
-          {(field, props) => (
-            <>
-              <Label>{t('USERS.FORM.CONFIRM_PASSWORD')}</Label>
-              <Input {...props} type='password' value={field.value} />
-            </>
-          )}
-        </Field>
+      <Field name='password'>
+        {(field, props) => (
+          <>
+            <Label>{t('AUTHENTICATION.RESET_PASSWORD.NEW_PASSWORD')}</Label>
+            <Input
+              {...props}
+              placeholder={t('AUTHENTICATION.RESET_PASSWORD.NEW_PASSWORD')}
+              type='password'
+              value={field.value}
+            />
+          </>
+        )}
+      </Field>
 
-        <Button type='submit'>
-          {t('AUTHENTICATION.RESET_PASSWORD.TITLE')}
-        </Button>
-      </Form>
-    </>
+      <Field name='passwordConfirmation'>
+        {(field, props) => (
+          <>
+            <Label>
+              {t('AUTHENTICATION.RESET_PASSWORD.CONFIRM_NEW_PASSWORD')}
+            </Label>
+            <Input
+              {...props}
+              placeholder={t(
+                'AUTHENTICATION.RESET_PASSWORD.CONFIRM_NEW_PASSWORD'
+              )}
+              type='password'
+              value={field.value}
+            />
+          </>
+        )}
+      </Field>
+
+      <Button type='submit'>{t('AUTHENTICATION.RESET_PASSWORD.TITLE')}</Button>
+    </Form>
   );
 };
 
