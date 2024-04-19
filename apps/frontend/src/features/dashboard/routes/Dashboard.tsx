475109616441f9c1c19d938b7941df10f7efc825
dawsta27 <dstaniszewski00@gmail.com>
2024-04-19 15:33:09 +0200

    feat(frontend): add dashboard prototype with fake data
    relates to B24-233

--- apps/frontend/src/features/dashboard/routes/Dashboard.tsx
+++ apps/frontend/src/features/dashboard/routes/Dashboard.tsx
@@ -1,18 +1,84 @@
 import Header from '@/components/layout/Header';
-import MapRailingLayer from '@/components/map/MapRailingLayer';
 import MapRoot from '@/components/map/MapRoot';
-import { Component } from 'solid-js';
+import { PieChart } from '@/components/ui/charts';
+import { Progress } from '@/components/ui/progress';
+import { useTranslations } from '@/features/i18n';
+import TripCard from '@/features/trips/components/TripCard';
+import { Component, For } from 'solid-js';
 
 const Dashboard: Component = () => {
+  const { t, d, n } = useTranslations();
+
+  const deviationChartData = {
+    labels: ['Loose bolt', 'Dent', 'Other'],
+    datasets: [
+      {
+        data: [6, 11, 1],
+      },
+    ],
+  };
+
+  //const trips = useTripsDetailsQuery();
+
   return (
     <>
       <Header />
-      <main class='flex justify-center'>
-        <div class=''>
-          <h2>Coverage</h2>
-          <MapRoot class='h-96 w-96'>
-            <MapRailingLayer></MapRailingLayer>
-          </MapRoot>
+      <main class='flex flex-col gap-2 px-32 py-2'>
+        <div class='flex flex-col'>
+          <p class='self-center text-xl'>
+            Plan X: 133/189 m captured
+          </p>
+          <Progress class='rounded-lg' value={80} />
+        </div>
+        <div>
+          <div class='grid grid-cols-2 grid-rows-2 gap-4'>
+            <section class='flex flex-col gap-2'>
+              <h2 class='text-2xl font-bold'>Map</h2>
+              <MapRoot class='h-full'></MapRoot>
+            </section>
+
+            <section class='flex flex-col gap-2'>
+              <h2 class='text-2xl font-bold'>Active trips</h2>
+              <TripCard
+                sequenceNumber={1}
+                startedAt={'04/10/24'}
+                endedAt={'24/10/24'}
+                deviations={4}
+                notes={2}
+                length={320}
+                car={'Toyota Corolla'}
+              />
+              <TripCard
+                sequenceNumber={2}
+                startedAt={'24/10/24'}
+                endedAt={'24/10/24'}
+                deviations={4}
+                notes={0}
+                length={320}
+                car={'Toyota Corolla'}
+              />
+              <TripCard
+                sequenceNumber={3}
+                startedAt={'24/10/24'}
+                endedAt={'24/10/24'}
+                deviations={4}
+                notes={0}
+                length={320}
+                car={'Toyota Corolla'}
+              />
+            </section>
+
+            <section class='flex flex-col gap-2'>
+              <h2 class='text-2xl font-bold'>Deviations</h2>
+              <div class='h-5/6 rounded-lg border border-gray-500 p-2'>
+                <PieChart data={deviationChartData} />
+              </div>
+            </section>
+
+            <section class='flex flex-col gap-2'>
+              <h2 class='text-2xl font-bold'>Something else</h2>
+            </section>
+          </div>
         </div>
       </main>
     </>
