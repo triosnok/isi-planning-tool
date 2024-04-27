01f197d538f6d31a9056279d19a432d083fe22ea
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-04-27 23:35:07 +0200

    fix(frontend): separate dark mode styles for resizable handle to make it render properly

--- apps/frontend/src/components/layout/Resizable.tsx
+++ apps/frontend/src/components/layout/Resizable.tsx
@@ -7,7 +7,7 @@ const ResizableHandle: Component = () => {
   const ctx = ResizablePrimitive.useContext();
 
   return (
-    <ResizablePrimitive.Handle class='data-[active]:bg-brand-blue-400 group relative z-10 flex w-px items-center justify-center bg-gray-300 transition-colors data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full max-md:hidden dark:bg-gray-800'>
+    <ResizablePrimitive.Handle class='data-[active]:bg-brand-blue-400 dark:data-[active]:bg-brand-blue-400 group relative z-10 flex w-px items-center justify-center bg-gray-300 transition-colors data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full max-md:hidden dark:bg-gray-800'>
       <div class='flex w-fit items-center justify-center rounded-sm border border-gray-400 bg-gray-300 py-1 dark:border-gray-800 dark:bg-gray-900'>
         <IconGripVertical class='size-4 group-data-[orientation=vertical]:rotate-90' />
       </div>
