7493692d0e5858d55d18109b1cc5ac8d36796116
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2024-05-01 14:49:03 +0200

    refactor(frontend): remove trip layout
    we don't need a separate layout file if its only one route

--- apps/frontend/src/features/trips/routes/Trip.tsx
+++ apps/frontend/src/features/trips/routes/Trip.tsx
@@ -33,6 +33,11 @@ import {
 import TripNoteModule from '../components/TripNoteModule';
 import TripSummaryDialog from '../components/TripSummaryDialog';
 import { ImageStatus, getImageAnalysis } from '../utils';
+import MapZoomControls from '@/components/map/MapZoomControls';
+import MapFollowVehicleToggle from '@/components/map/MapFollowVehicleToggle';
+import MapPopupLayer from '@/components/map/MapPopupLayer';
+import Header from '@/components/layout/Header';
+import MapRoot from '@/components/map/MapRoot';
 
 const Trip: Component = () => {
   const params = useParams();
@@ -116,171 +121,187 @@ const Trip: Component = () => {
   };
 
   return (
-    <>
-      <div class='pointer-events-none absolute left-0 top-0 flex h-full w-full flex-col gap-2 overflow-hidden max-md:flex-col-reverse md:w-1/2 md:p-4 lg:w-2/5 xl:w-1/3'>
-        <section class='pointer-events-auto bg-gray-50 p-2 max-md:rounded-t-lg md:rounded-md dark:bg-gray-900'>
-          <div class='flex flex-col'>
-            <div class='hidden md:flex'>
-              <BackLink href='../..' />
-            </div>
-          </div>
-          <div class='space-y-2'>
-            <div class='flex flex-col justify-between space-y-2 md:flex-row md:flex-wrap'>
-              <div class='order-first md:order-none'>
-                <h1 class='text-3xl font-bold'>
-                  {project.data?.name} - {t('TRIPS.TRIP')}{' '}
-                  {tripDetails.data?.sequenceNumber}
-                </h1>
-                <span class='text-sm'>
-                  {d(project.data?.startsAt, DateFormat.MONTH_DAY)}
-                  <Show when={dayjs(project.data?.endsAt).isValid()}>
-                    {` - ${d(project.data?.endsAt, DateFormat.MONTH_DAY)}`}
-                  </Show>
-                </span>
+    <div class='flex h-svh w-svw flex-col'>
+      <Header />
+
+      <MapRoot class='relative flex-1'>
+        <div class='pointer-events-none absolute left-0 top-0 flex h-full w-full flex-col gap-2 overflow-hidden max-md:flex-col-reverse md:w-1/2 md:p-4 lg:w-2/5 xl:w-1/3'>
+          <section class='pointer-events-auto bg-gray-50 p-2 max-md:rounded-t-lg md:rounded-md dark:bg-gray-900'>
+            <div class='flex flex-col'>
+              <div class='hidden md:flex'>
+                <BackLink href='../..' />
               </div>
-              <Button
-                onClick={() => setShowTripNoteModule(!showTripNoteModule())}
-                class={cn(
-                  'order-last md:order-none',
-                  showTripNoteModule() &&
-                    'bg-success-600 hover:bg-success-600/90'
-                )}
-              >
-                <div class='flex items-center gap-2'>
-                  <IconMessage />
-                  <p class='md:hidden'>{t('NOTES.ADD_NOTE')}</p>
+            </div>
+
+            <div class='space-y-2'>
+              <div class='flex flex-col justify-between space-y-2 md:flex-row md:flex-wrap'>
+                <div class='order-first md:order-none'>
+                  <h1 class='text-3xl font-bold'>
+                    {project.data?.name} - {t('TRIPS.TRIP')}{' '}
+                    {tripDetails.data?.sequenceNumber}
+                  </h1>
+                  <span class='text-sm'>
+                    {d(project.data?.startsAt, DateFormat.MONTH_DAY)}
+                    <Show when={dayjs(project.data?.endsAt).isValid()}>
+                      {` - ${d(project.data?.endsAt, DateFormat.MONTH_DAY)}`}
+                    </Show>
+                  </span>
                 </div>
+                <Button
+                  onClick={() => setShowTripNoteModule(!showTripNoteModule())}
+                  class={cn(
+                    'order-last md:order-none',
+                    showTripNoteModule() &&
+                      'bg-success-600 hover:bg-success-600/90'
+                  )}
+                >
+                  <div class='flex items-center gap-2'>
+                    <IconMessage />
+                    <p class='md:hidden'>{t('NOTES.ADD_NOTE')}</p>
+                  </div>
+                </Button>
+
+                <div class='order-2 w-full text-center md:order-none'>
+                  <Progress class='rounded-lg' value={progressPercent()} />
+                  <p>
+                    {n(progressValue(), NumberFormat.INTEGER)}
+                    {' / '}
+                    {n(project.data?.totalLength, NumberFormat.INTEGER)}
+                    {' m'}
+                  </p>
+                </div>
+              </div>
+              <section class='grid grid-cols-2 gap-2 text-sm md:grid-cols-4'>
+                <Indicator
+                  variant={storageIndicator()}
+                  icon={IconDatabase}
+                  indicates='Storage left'
+                  status={n(
+                    captureDetails()?.storageRemaining,
+                    NumberFormat.PERCENTAGE
+                  )}
+                />
+
+                <Indicator
+                  variant={gpsIndicator()}
+                  icon={IconCurrentLocation}
+                  indicates='GPS'
+                  status={n(
+                    captureDetails()?.gpsSignal,
+                    NumberFormat.PERCENTAGE
+                  )}
+                />
+
+                <Indicator
+                  variant={
+                    captureDetails()?.activeCapture
+                      ? IndicatorVariant.SUCCESS
+                      : IndicatorVariant.UNDETERMINED
+                  }
+                  icon={IconVideo}
+                  indicates='Capture'
+                  status={
+                    captureDetails()?.activeCapture ? 'Active' : 'Inactive'
+                  }
+                />
+
+                <Indicator
+                  variant={imageIndicator()}
+                  icon={IconPhoto}
+                  indicates='Images'
+                  status=''
+                />
+              </section>
+
+              <Collapsible.Root class='flex flex-col gap-2'>
+                <Collapsible.Content class='animate-collapsible-up ui-expanded:animate-collapsible-down overflow-hidden'>
+                  <div class='flex flex-row items-center gap-2 px-2'>
+                    <SwitchButton
+                      checked={hideCompletedRailings()}
+                      onChange={setHideCompletedRailings}
+                    />
+                    <p>{t('RAILINGS.HIDE_COMPLETED_RAILINGS')}</p>
+                  </div>
+                </Collapsible.Content>
+
+                <Collapsible.Trigger
+                  onClick={() => setShowMore((v) => !v)}
+                  class='group flex flex-row items-center justify-center gap-1 rounded-md bg-gray-100 p-1 dark:bg-gray-800'
+                >
+                  <IconChevronUp class='size-6 transform transition-transform group-data-[closed]:rotate-180' />
+
+                  <Show when={showMore()} fallback={t('TRIPS.SHOW_MORE')}>
+                    {t('TRIPS.SHOW_LESS')}
+                  </Show>
+                </Collapsible.Trigger>
+              </Collapsible.Root>
+
+              <TripSummaryDialog
+                tripId={params.tripId}
+                open={showSummaryDialog()}
+                onOpenChange={setShowSummaryDialog}
+                captureDetails={captureDetails()}
+              />
+
+              <Show when={!tripDetails.data?.endedAt}>
+                <Button
+                  onClick={() => setShowSummaryDialog(true)}
+                  variant='destructive'
+                  class='w-full md:hidden'
+                >
+                  {t('TRIPS.END_TRIP')}
+                </Button>
+              </Show>
+            </div>
+          </section>
+
+          <div class='relative flex-1 overflow-hidden max-md:hidden'>
+            <Show when={showTripNoteModule()}>
+              <TripNoteModule
+                tripId={params.tripId}
+                open={showTripNoteModule()}
+                onOpenChange={setShowTripNoteModule}
+              />
+            </Show>
+          </div>
+
+          <Show when={!tripDetails.data?.endedAt}>
+            <div class='pointer-events-auto w-full space-y-2 rounded-md bg-gray-50 p-2 max-md:hidden dark:bg-gray-950'>
+              <Button onClick={handleCaptureAction} class='w-full'>
+                <Show
+                  when={!captureDetails()?.activeCapture}
+                  fallback={<span>{t('TRIPS.STOP_CAPTURE')}</span>}
+                >
+                  <span>{t('TRIPS.START_CAPTURE')}</span>
+                </Show>
               </Button>
 
-              <div class='order-2 w-full text-center md:order-none'>
-                <Progress class='rounded-lg' value={progressPercent()} />
-                <p>
-                  {n(progressValue(), NumberFormat.INTEGER)}
-                  {' / '}
-                  {n(project.data?.totalLength, NumberFormat.INTEGER)}
-                  {' m'}
-                </p>
-              </div>
-            </div>
-            <section class='grid grid-cols-2 gap-2 text-sm md:grid-cols-4'>
-              <Indicator
-                variant={storageIndicator()}
-                icon={IconDatabase}
-                indicates='Storage left'
-                status={n(
-                  captureDetails()?.storageRemaining,
-                  NumberFormat.PERCENTAGE
-                )}
-              />
-
-              <Indicator
-                variant={gpsIndicator()}
-                icon={IconCurrentLocation}
-                indicates='GPS'
-                status={n(captureDetails()?.gpsSignal, NumberFormat.PERCENTAGE)}
-              />
-
-              <Indicator
-                variant={
-                  captureDetails()?.activeCapture
-                    ? IndicatorVariant.SUCCESS
-                    : IndicatorVariant.UNDETERMINED
-                }
-                icon={IconVideo}
-                indicates='Capture'
-                status={captureDetails()?.activeCapture ? 'Active' : 'Inactive'}
-              />
-
-              <Indicator
-                variant={imageIndicator()}
-                icon={IconPhoto}
-                indicates='Images'
-                status=''
-              />
-            </section>
-
-            <Collapsible.Root class='flex flex-col gap-2'>
-              <Collapsible.Content class='animate-collapsible-up ui-expanded:animate-collapsible-down overflow-hidden'>
-                <div class='flex flex-row items-center gap-2 px-2'>
-                  <SwitchButton
-                    checked={hideCompletedRailings()}
-                    onChange={setHideCompletedRailings}
-                  />
-                  <p>{t('RAILINGS.HIDE_COMPLETED_RAILINGS')}</p>
-                </div>
-              </Collapsible.Content>
-
-              <Collapsible.Trigger
-                onClick={() => setShowMore((v) => !v)}
-                class='group flex flex-row items-center justify-center gap-1 rounded-md bg-gray-100 p-1 dark:bg-gray-800'
-              >
-                <IconChevronUp class='size-6 transform transition-transform group-data-[closed]:rotate-180' />
-
-                <Show when={showMore()} fallback={t('TRIPS.SHOW_MORE')}>
-                  {t('TRIPS.SHOW_LESS')}
-                </Show>
-              </Collapsible.Trigger>
-            </Collapsible.Root>
-
-            <TripSummaryDialog
-              tripId={params.tripId}
-              open={showSummaryDialog()}
-              onOpenChange={setShowSummaryDialog}
-              captureDetails={captureDetails()}
-            />
-
-            <Show when={!tripDetails.data?.endedAt}>
               <Button
                 onClick={() => setShowSummaryDialog(true)}
                 variant='destructive'
-                class='w-full md:hidden'
+                class='flex w-full'
               >
                 {t('TRIPS.END_TRIP')}
               </Button>
-            </Show>
-          </div>
-        </section>
-
-        <div class='relative flex-1 overflow-hidden max-md:hidden'>
-          <Show when={showTripNoteModule()}>
-            <TripNoteModule
-              tripId={params.tripId}
-              open={showTripNoteModule()}
-              onOpenChange={setShowTripNoteModule}
-            />
+            </div>
           </Show>
         </div>
 
-        <Show when={!tripDetails.data?.endedAt}>
-          <div class='pointer-events-auto w-full space-y-2 rounded-md bg-gray-50 p-2 max-md:hidden dark:bg-gray-950'>
-            <Button onClick={handleCaptureAction} class='w-full'>
-              <Show
-                when={!captureDetails()?.activeCapture}
-                fallback={<span>{t('TRIPS.STOP_CAPTURE')}</span>}
-              >
-                <span>{t('TRIPS.START_CAPTURE')}</span>
-              </Show>
-            </Button>
-
-            <Button
-              onClick={() => setShowSummaryDialog(true)}
-              variant='destructive'
-              class='flex w-full'
-            >
-              {t('TRIPS.END_TRIP')}
-            </Button>
-          </div>
+        <Show when={captureDetails()}>
+          {(dt) => (
+            <MapCarLayer heading={dt().heading} position={dt().position} />
+          )}
         </Show>
-      </div>
 
-      <Show when={captureDetails()}>
-        {(dt) => (
-          <MapCarLayer heading={dt().heading} position={dt().position} />
-        )}
-      </Show>
+        <MapPopupLayer />
+        <MapRailingLayer railings={railings.data} />
 
-      <MapRailingLayer railings={railings.data} />
-    </>
+        <div class='absolute right-2 top-2 flex flex-col gap-2'>
+          <MapZoomControls />
+          <MapFollowVehicleToggle />
+        </div>
+      </MapRoot>
+    </div>
   );
 };
 
