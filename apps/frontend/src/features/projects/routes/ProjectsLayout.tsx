0f16ac69e385b3aa7923e136a3127e81a02d8f4a
GBFur <zifergames@gmail.com>
2024-05-06 14:05:20 +0200

    feat(frontend): add mobile tab bar to toggle between map and project view for `ProjectsLayout`
    related to B24-308

--- apps/frontend/src/features/projects/routes/ProjectsLayout.tsx
+++ apps/frontend/src/features/projects/routes/ProjectsLayout.tsx
@@ -1,13 +1,14 @@
 import Header from '@/components/layout/Header';
+import MapPopupLayer from '@/components/map/MapPopupLayer';
 import MapRailingLayer from '@/components/map/MapRailingLayer';
 import MapRoot from '@/components/map/MapRoot';
 import MapZoomControls from '@/components/map/MapZoomControls';
-import { LayoutProps } from '@/lib/utils';
+import { LayoutProps, cn } from '@/lib/utils';
 import { useParams } from '@solidjs/router';
-import { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { useProjectRailings } from '../api';
+import ProjectTabBar from '../components/ProjectTabBar';
 import { useProjectSearchParams } from '../utils';
-import MapPopupLayer from '@/components/map/MapPopupLayer';
 
 const ProjectsLayout: Component<LayoutProps> = (props) => {
   const params = useParams();
@@ -18,16 +19,28 @@ const ProjectsLayout: Component<LayoutProps> = (props) => {
     searchParams.hideCompleted
   );
 
+  const [showMobileMap, setShowMobileMap] = createSignal(false);
+
   return (
     <div class='flex h-svh w-svw flex-col overflow-hidden'>
       <Header />
 
       <main class='flex flex-1 overflow-hidden'>
-        <aside class='flex w-full flex-shrink-0 flex-col overflow-hidden md:w-96'>
+        <aside
+          class={cn(
+            'flex w-full flex-shrink-0 flex-col overflow-hidden md:w-96',
+            showMobileMap() && 'max-md:hidden'
+          )}
+        >
           {props.children}
         </aside>
 
-        <div class='flex-1 max-md:hidden'>
+        <div
+          class={cn(
+            'flex-1',
+            showMobileMap() ? 'max-md:flex' : 'max-md:hidden'
+          )}
+        >
           <MapRoot class='relative h-full w-full' customZoom>
             <MapZoomControls class='absolute right-2 top-2' />
             <MapRailingLayer railings={railings.data} />
@@ -35,6 +48,21 @@ const ProjectsLayout: Component<LayoutProps> = (props) => {
           </MapRoot>
         </div>
       </main>
+
+      <div class='md:hidden'>
+        <ProjectTabBar
+          tabs={{
+            map: {
+              isActive: showMobileMap(),
+              onPress: () => setShowMobileMap(true),
+            },
+            project: {
+              isActive: !showMobileMap(),
+              onPress: () => setShowMobileMap(false),
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
