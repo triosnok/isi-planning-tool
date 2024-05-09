a5e793f08409f096d88f195246eadf834c693d50
GBFur <zifergames@gmail.com>
2024-05-09 14:40:02 +0200

    feat(backend): make `imageUrl` optional for vehicle requests & remove URL constraint

--- packages/client/src/main/java/no/isi/insight/planning/client/vehicle/view/UpdateVehicleRequest.java
+++ packages/client/src/main/java/no/isi/insight/planning/client/vehicle/view/UpdateVehicleRequest.java
@@ -1,15 +1,14 @@
 package no.isi.insight.planning.client.vehicle.view;
 
 import java.time.LocalDate;
-
-import org.hibernate.validator.constraints.URL;
+import no.isi.insight.planning.client.annotation.Optional;
 
 import jakarta.validation.constraints.NotBlank;
 import no.isi.insight.planning.client.annotation.GenerateTypeScript;
 
 @GenerateTypeScript
 public record UpdateVehicleRequest(
-  @URL String imageUrl,
+  @Optional String imageUrl,
   @NotBlank String registrationNumber,
   @NotBlank String model,
   Boolean camera,
